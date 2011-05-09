dcc07313e69f7b78d66c5ec28d38a7ed1c3baf21
Justin Labak <jtlabak@gmail.com>
2011-05-08 22:16:15 -0700

    Added some objects and a free movement camera that still needs help

---
+++ Assets/Scripts/FreeCamera.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using System.Collections;
+
+public class FreeCamera : MonoBehaviour {
+	
+	public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+	public RotationAxes axes = RotationAxes.MouseXAndY;
+	public float sensitivityX = 15F;
+	public float sensitivityY = 15F;
+
+	public float minimumX = -360F;
+	public float maximumX = 360F;
+
+	public float minimumY = -60F;
+	public float maximumY = 60F;
+
+	float rotationY = 0F;
+	
+	private bool locked;
+
+	// Use this for initialization
+	void Start () {
+		this.LockLook();
+	}
+	
+	public void LockLook() {
+		Screen.lockCursor = true;	
+		this.locked = true;
+	}
+	
+	public void UnlockLook() {
+		Screen.lockCursor = false;
+		this.locked = false;
+	}
+	
+	public void ToggleLook() {
+		this.locked = !this.locked;
+		Screen.lockCursor = this.locked;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (locked) {
+			
+			float speed = (Input.GetKey(KeyCode.LeftShift)) ? 30 : 10;
+			speed *= Time.deltaTime;
+			
+			// keyboard input
+			if (Input.GetKey(KeyCode.W)) {
+				transform.Translate(transform.forward.normalized * speed);	
+			}
+			if (Input.GetKey(KeyCode.A)) {
+				transform.Translate(-transform.right.normalized * speed);	
+			}
+			if (Input.GetKey(KeyCode.S)) {
+				transform.Translate(-transform.forward.normalized * speed);	
+			}
+			if (Input.GetKey(KeyCode.D)) {
+				transform.Translate(transform.right.normalized * speed);	
+			}
+			
+			if (axes == RotationAxes.MouseXAndY)
+			{
+				float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+				
+				rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+				rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+				
+				transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+			}
+			else if (axes == RotationAxes.MouseX)
+			{
+				transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+			}
+			else
+			{
+				rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+				rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+				
+				transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/InputCmd.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class InputCmd : MonoBehaviour {
+
+	private GameObject cam;
+
+	// Use this for initialization
+	void Start () {
+		this.cam = GameObject.Find("Camera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Tab)) {
+			this.cam.GetComponent<FreeCamera>().ToggleLook();
+		} else if (Input.GetKeyDown(KeyCode.Escape)) {
+			this.cam.GetComponent<FreeCamera>().UnlockLook();
+		}
+	}
+}
